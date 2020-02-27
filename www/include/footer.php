72df903a268649505cee07d8690ac58ed8609cd1
ThomasGhignon <ghignon.thomas@gmail.com>
2020-02-27 17:30:09 +0100

    up

---
+++ www/include/footer.php
@@ -0,0 +1,39 @@
+<footer>
+  <div class="wrap">
+    <div>
+      <div class="returnTop">
+        <div>
+          <img src="img/logo/white-moabi-simple.png" alt="Logo moabi">
+          <a href="#">moabi</a>
+        </div>
+      </div>
+      <div class="coordinator">
+        <h4>BURGARD Thomas</h4>
+        <div>
+          <img src="img/icon/icon-footer-email.svg" alt="Email">
+          <p>burgard.thomas.dcr@gmail.com</p>
+        </div>
+        <div>
+          <img src="img/icon/icon-footer-phone.svg" alt="Numéro de téléphone">
+          <p>06 07 49 82 52</p>
+        </div>
+      </div>
+      <div class="coordinator">
+        <h4>REYMOND Emma</h4>
+        <div>
+          <img src="img/icon/icon-footer-email.svg" alt="Email">
+          <p>emmareymond66@gmail.com</p>
+        </div>
+        <div>
+          <img src="img/icon/icon-footer-phone.svg" alt="Numéro de téléphone">
+          <p>07 88 37 54 87</p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <img src="img/logo/white-IUT-chambery-USMB.png" alt="Logo USMB">
+      <img src="img/logo/white-mmi.png" alt="Logo MMI">
+      <a href="#">Mentions légales</a>
+    </div>
+  </div>
+</footer>
--- www/index.php
+++ www/index.php
@@ -10,9 +10,10 @@
   <link rel="stylesheet" href="css/libs/cssReset.css">
   <link rel="stylesheet" href="css/style_index.css">
   <link rel="stylesheet" href="css/style_global.css">
+
+  <link rel="stylesheet" href="css/style_footer.css">
 </head>
 <body>
-
   <header>
     <div class="wrap">
       <nav>
@@ -40,144 +41,143 @@
       </div>
     </div>
   </header>
-  <div class="main">
-    <div class="wrap">
-      <div class="main_container">
-        <div class="line outside-line">
+  <div class="wrap">
+    <div class="main_container">
+      <div class="line outside-line">
+        <div class="lineContent"></div>
+        <div class="line inside1-line">
           <div class="lineContent"></div>
-          <div class="line inside1-line">
+          <div class="line inside2-line">
             <div class="lineContent"></div>
-            <div class="line inside2-line">
-              <div class="lineContent"></div>
-              <div class="lineContent js-offsetLine1"></div>
-              <div class="lineContent js-offsetLine2"></div>
-            </div>
-            <div class="lineContent"></div>
+            <div class="lineContent js-offsetLine1"></div>
+            <div class="lineContent js-offsetLine2"></div>
           </div>
           <div class="lineContent"></div>
         </div>
-          <section class="agency">
-            <div class="agencyValue_container">
-              <div class="background_whiteLogo">
-                <img src="img/logo/white-moabi-onlyFigure.svg" alt="Logo Moabi">
-              </div>
-              <div class="agencyValue_content">
-                <img src="img/logo/orange-moabi.svg" alt="Logo Moabi">
-                <h2>notre agence</h2>
-                <p>Le brin de curiosité mêlé à un soupçon d’audace qui vous donnera la recette pour aller au bout de vos projets.
-                Venez goûter le fruit de notre passion!</p>
-                <p>Moabi est une agence de 15 étudiants de deuxième années en DUT MMI.  Passionné et complémentaire, ils sont en charge de réaliser différents projets alliant de multiples compétences tout au long de l’année.</p>
-                <a href="#">
-                  <span>rapport d'agence</span></a>
-              </div>
-            </div>
-            <div class="agencySkills_container">
-              <h2>nos domaines</h2>
-              <div class="agencySkills_content">
-                <div>
-                  <img src="img/icon/icon-bubble.png" alt="css/Bulle de dialogue">
-                  <p>Qu'elle soit digital ou non, nous l’assurons pour propulser vos projets.</p>
-                </div>
-                <div>
-                  <img src="img/icon/icon-computer.png" alt="Ordinateur">
-                  <p>Les lignes de code dansent grâce à nous pour s’aligner comme dans vos pensées.</p>
-                </div>
-                <div>
-                  <img src="img/icon/icon-pen.png" alt="Stylo">
-                  <p>L’univers graphique est vaste, nous sommes là pour vous guider.</p>
-                </div>
-                <div>
-                  <img src="img/icon/icon-camera.png" alt="Camera">
-                  <p>Nous jouons avec l’image et le son pour sublimer vos projets.</p>
-                </div>
-              </div>
-            </div>
-          </section>
-          <section class="project">
-            <h2>nos projets</h2>
-            <div class="project-right_content">
-              <span class="project_title">
-                <h3>Naturopathe</h3>
-              </span>
-              <div class="project_img_1 js-project_img"></div>
-              <div class="projet-desc_container">
-                <div class="js-desc_content">
-                  <p>Conception et réalisation d’un univers graphique et d’un site web pour le compte d’une naturopathe indépendante.</p>
-                  <a href="#">Voir le projet ></a>
-                </div>
-              </div>
-            </div>
-
-            <div class="project-left_content">
-              <span class="project_title">
-                <h3>Myrelingue</h3>
-              </span>
-              <div class="projet-desc_container">
-                <div class="js-desc_content">
-                  <p>Réalisation de motions design pour prévenir les méthodes de blanchiment de la peau en Afrique.</p>
-                  <a href="#">Voir le projet ></a>
-                </div>
-              </div>
-              <div class="project_img_1 js-project_img"></div>
-            </div>
-
-            <div class="project-right_content">
-              <span class="project_title">
-                <h3>FRAMMI</h3>
-              </span>
-              <div class="project_img_1 js-project_img"></div>
-              <div class="projet-desc_container">
-                <div class="js-desc_content">
-                  <p>Organisation du festival audiovisuel vitrine du département MMI, qui récompense les réalisation des élèves de ce département au travers d’une cérémonie regroupant élèves, professeurs et professionnels.</p>
-                  <a href="#">Voir le projet ></a>
-                </div>
-              </div>
-            </div>
-
-            <div class="project-left_content">
-              <span class="project_title">
-                <h3>Vital coach <br> envent</h3>
-              </span>
-              <div class="projet-desc_container">
-                <div class="js-desc_content">
-                  <p>Réalisation d’une vidéo promotionnel pour l'événement “ Road E-Bike Grand Prix” et par la même occasion, faire connaître le monde du vélo à assistance électrique en compétition.</p>
-                  <a href="#">Voir le projet ></a>
-                </div>
-              </div>
-              <div class="project_img_1 js-project_img"></div>
-            </div>
-
-            <div class="project-right_content">
-              <span class="project_title">
-                <h3>Joseph Hakka</h3>
-              </span>
-              <div class="project_img_1 js-project_img"></div>
-              <div class="projet-desc_container">
-                <div class="js-desc_content">
-                  <p>Conception et réalisation, en relation avec l’artiste Joseph Aka, d’un environnement numérique pour la création d’un spectacle de danse contemporaine sur le sujet de la migration.</p>
-                  <a href="#">Voir le projet ></a>
-                </div>
-              </div>
-            </div>
-
-            <div class="project-left_content">
-              <span class="project_title">
-                <h3>IFSI Escape <br> Game</h3>
-              </span>
-              <div class="projet-desc_container">
-                <div class="js-desc_content">
-                  <p>Conception et réalisation d’un escape game semi-numérique autour du thème de la gestion des risques lors des transfusions sanguines.</p>
-                  <a href="#">Voir le projet ></a>
-                </div>
-              </div>
-              <div class="project_img_1 js-project_img"></div>
-            </div>
-
-          </section>
-        </div>
+        <div class="lineContent"></div>
       </div>
-      <script src="js/libs/jquery.js"></script>
-      <script src="js/index_imgSize.js"></script>
-      <script src="js/index-projet_offsetTxt.js"></script>
-    </body>
-    </html>
+      <section class="agency">
+        <div class="agencyValue_container">
+          <div class="background_whiteLogo">
+            <img src="img/logo/white-moabi-onlyFigure.svg" alt="Logo Moabi">
+          </div>
+          <div class="agencyValue_content">
+            <img src="img/logo/orange-moabi.svg" alt="Logo Moabi">
+            <h2>notre agence</h2>
+            <p>Le brin de curiosité mêlé à un soupçon d’audace qui vous donnera la recette pour aller au bout de vos projets.
+            Venez goûter le fruit de notre passion!</p>
+            <p>Moabi est une agence de 15 étudiants de deuxième années en DUT MMI.  Passionné et complémentaire, ils sont en charge de réaliser différents projets alliant de multiples compétences tout au long de l’année.</p>
+            <a href="#">
+              <span>rapport d'agence</span>
+            </a>
+          </div>
+        </div>
+        <div class="agencySkills_container">
+          <h2>nos domaines</h2>
+          <div class="agencySkills_content">
+            <div>
+              <img src="img/icon/icon-bubble.png" alt="css/Bulle de dialogue">
+              <p>Qu'elle soit digital ou non, nous l’assurons pour propulser vos projets.</p>
+            </div>
+            <div>
+              <img src="img/icon/icon-computer.png" alt="Ordinateur">
+              <p>Les lignes de code dansent grâce à nous pour s’aligner comme dans vos pensées.</p>
+            </div>
+            <div>
+              <img src="img/icon/icon-pen.png" alt="Stylo">
+              <p>L’univers graphique est vaste, nous sommes là pour vous guider.</p>
+            </div>
+            <div>
+              <img src="img/icon/icon-camera.png" alt="Camera">
+              <p>Nous jouons avec l’image et le son pour sublimer vos projets.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="project">
+        <h2>nos projets</h2>
+        <div class="project-right_content">
+          <span class="project_title">
+            <h3>Naturopathe</h3>
+          </span>
+          <div class="project_img_1 js-project_img"></div>
+          <div class="projet-desc_container">
+            <div class="js-desc_content">
+              <p>Conception et réalisation d’un univers graphique et d’un site web pour le compte d’une naturopathe indépendante.</p>
+              <a href="#">Voir le projet ></a>
+            </div>
+          </div>
+        </div>
+        <div class="project-left_content">
+          <span class="project_title">
+            <h3>Myrelingue</h3>
+          </span>
+          <div class="projet-desc_container">
+            <div class="js-desc_content">
+              <p>Réalisation de motions design pour prévenir les méthodes de blanchiment de la peau en Afrique.</p>
+              <a href="#">Voir le projet ></a>
+            </div>
+          </div>
+          <div class="project_img_1 js-project_img"></div>
+        </div>
+        <div class="project-right_content">
+          <span class="project_title">
+            <h3>FRAMMI</h3>
+          </span>
+          <div class="project_img_1 js-project_img"></div>
+          <div class="projet-desc_container">
+            <div class="js-desc_content">
+              <p>Organisation du festival audiovisuel vitrine du département MMI, qui récompense les réalisation des élèves de ce département au travers d’une cérémonie regroupant élèves, professeurs et professionnels.</p>
+              <a href="#">Voir le projet ></a>
+            </div>
+          </div>
+        </div>
+        <div class="project-left_content">
+          <span class="project_title">
+            <h3>Vital coach <br> envent</h3>
+          </span>
+          <div class="projet-desc_container">
+            <div class="js-desc_content">
+              <p>Réalisation d’une vidéo promotionnel pour l'événement “ Road E-Bike Grand Prix” et par la même occasion, faire connaître le monde du vélo à assistance électrique en compétition.</p>
+              <a href="#">Voir le projet ></a>
+            </div>
+          </div>
+          <div class="project_img_1 js-project_img"></div>
+        </div>
+        <div class="project-right_content">
+          <span class="project_title">
+            <h3>Joseph Hakka</h3>
+          </span>
+          <div class="project_img_1 js-project_img"></div>
+          <div class="projet-desc_container">
+            <div class="js-desc_content">
+              <p>Conception et réalisation, en relation avec l’artiste Joseph Aka, d’un environnement numérique pour la création d’un spectacle de danse contemporaine sur le sujet de la migration.</p>
+              <a href="#">Voir le projet ></a>
+            </div>
+          </div>
+        </div>
+        <div class="project-left_content">
+          <span class="project_title">
+            <h3>IFSI Escape <br> Game</h3>
+          </span>
+          <div class="projet-desc_container">
+            <div class="js-desc_content">
+              <p>Conception et réalisation d’un escape game semi-numérique autour du thème de la gestion des risques lors des transfusions sanguines.</p>
+              <a href="#">Voir le projet ></a>
+            </div>
+          </div>
+          <div class="project_img_1 js-project_img"></div>
+        </div>
+      </section>
+      <section class="members">
+        <h2>nos membres</h2>
+        <div></div>
+      </section>
+    </div>
+  </div>
+  <?php include 'include/footer.php'; ?>
+    <script src="js/libs/jquery.js"></script>
+    <script src="js/index_imgSize.js"></script>
+    <script src="js/index-projet_offsetTxt.js"></script>
+</body>
+</html>
